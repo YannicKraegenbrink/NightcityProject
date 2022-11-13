053f7ccc9a4e7c50c7ce02880e54d812af3ac090
Phoenyx <kraegenbrink@sport-thieme.de>
2022-11-13 23:50:25 +0100

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+//burgerMenu
+function BurgerOpen() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
